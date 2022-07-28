f8288382c38f47d4d1c13c82f20860b632930509
JosephPM39 <josias.jjpm49@hotmail.com>
2022-07-27 21:04:59 -0600

    My first test

---
+++ demos/src/02-math.js
@@ -0,0 +1,26 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  //To evite errors, this should be uncommented
+  //if(b === 0){
+    //return null;
+  //}
+  return a / b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+module.exports = {
+  sum,
+  multiply,
+  divide,
+  subtract
+}
