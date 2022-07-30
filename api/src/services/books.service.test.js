c5b99b63f59615acd1afbd3835b0ccf0d4bf18ea
JosephPM39 <josias.jjpm49@hotmail.com>
2022-07-30 15:26:50 -0600

    Mocking with jest, and use Stubs, Fakes, and dummy basics

--- api/src/services/books.service.test.js
+++ api/src/services/books.service.test.js
@@ -1,10 +1,38 @@
 const BooksService = require('./books.service');
 
+// Fake
+const fakeBooks = [
+  // Dummy
+  {
+    _id: 1,
+    name: 'Harry Potter',
+  },
+];
+
+// Stub
+const MongoLibStub = {
+  getAll: () => [...fakeBooks],
+  create: () => {},
+};
+
+// This is form to use Mocks with jest
+jest.mock(
+  // When the code calls this library
+  '../lib/mongo.lib',
+  // Impersonate it with MongoLibStub
+  () => jest.fn().mockImplementation(
+    () => MongoLibStub,
+  ),
+);
+
 describe('Test for BooksService', () => {
   // Arrange
   let service;
   beforeEach(() => {
     service = new BooksService();
+    // This function is very useful to use new info
+    // in each test
+    jest.clearAllMocks();
   });
 
   describe('Test for getBooks', () => {
@@ -13,7 +41,15 @@ describe('Test for BooksService', () => {
       const books = await service.getBooks({});
       console.log(books);
       // Assert
-      expect(books.length).toEqual(2);
+      expect(books.length).toEqual(1);
+    });
+
+    test('Should return a list book', async () => {
+      // Act
+      const books = await service.getBooks({});
+      console.log(books);
+      // Assert
+      expect(books[0].name).toEqual('Harry Potter');
     });
   });
 });
